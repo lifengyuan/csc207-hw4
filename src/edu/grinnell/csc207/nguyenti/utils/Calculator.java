4da6cca8711b11a080432fb3050c371808852b21
wheelere <earnestw@Earnests-MacBook-Air.local>
2013-09-21 15:15:57 -0500

    changed o

--- src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
+++ src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
 	String[] vals = expression.split(" ");
 	Fraction total;
 	if (!Character.isDigit(vals[0].charAt(0))) {
-	    throw new Exception("NOT A DIGIT, FOOL");
+	    throw new Exception("NOT A DIGIT, FOoL");
 	}
 	for (int i = 0; i < vals.length; i++) {
 	    for (int j = 0; j < vals[i].length(); j++) {
