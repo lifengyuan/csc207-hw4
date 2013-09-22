6c20ce94a140db91fb7163e8cf776c274495e241
nguyenti <tn.xc.lax@gmail.com>
2013-09-22 17:46:20 -0500

    Start implementation of Calculator

--- src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
+++ src/edu/grinnell/csc207/nguyenti/utils/Calculator.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.nguyenti.utils;
 
+import java.math.BigInteger;
 
 /**
  * Calculator implementation
@@ -11,17 +12,58 @@ package edu.grinnell.csc207.nguyenti.utils;
 
 public class Calculator {
 
-    String[] r = new String[9];
+    Fraction ZERO_FRACTION = new Fraction(0);
+    // r is an array of 10 fractions, each initialized as Fraction(0)
+    Fraction[] r = { ZERO_FRACTION, ZERO_FRACTION, ZERO_FRACTION,
+	    ZERO_FRACTION, ZERO_FRACTION, ZERO_FRACTION, ZERO_FRACTION,
+	    ZERO_FRACTION, ZERO_FRACTION, ZERO_FRACTION };
 
+    // HAVENT DEALT WITH "r0 =" case
     public Fraction evaluate(String expression) throws Exception {
 	String[] vals = expression.split(" ");
+	String revised = "";
+	int len = vals.length;
 	Fraction total;
-	if (!Character.isDigit(vals[0].charAt(0))) {
-	    throw new Exception("NOT A DIGIT, FOoL");
-	}
-	for (int i = 0; i < vals.length; i++) {
-	    for (int j = 0; j < vals[i].length(); j++) {
-		
+	char c = vals[0].charAt(0);
+	Fraction left;
+	Fraction right;
+	boolean operation = false;
+	if (!Character.isDigit(c) && c != 'r') {
+	    throw new Exception(vals[0]
+		    + " is not a digit or a storage element");
+	} else if (vals[1] == "=") {
+	    if (c != 'r') {
+		throw new Exception(vals[0] + " is not a storage element");
+	    } else if (vals[0].length() > 2
+		    && !Character.isDigit(vals[0].charAt(1))) {
+		throw new Exception(vals[0]
+			+ " is not a proper storage element");
+	    } else {
+		for (int i = 2; i < len; i++) {
+		    revised = revised + vals[i] + " "; // Worth checking
+		}
+		r[vals[0].charAt(1)] = evaluate(revised);
+	    }
+	} else {
+	    for (int j = 0; j < len; j++) {
+		if(vals[j].length() == 1) {
+		    switch (vals[j].charAt(0)) {
+		    case '+':
+		    case '-':
+		    case '*':
+		    case '/':
+		    case '^': 
+			operation = true;
+		    break;	
+		    default:  
+			operation = false;
+		    break;
+		    }
+		}
+		if(!operation) {
+		    left = Fraction(vals[j]);
+		}
+		c = vals[j].charAt[0]
 	    }
 	}
 	return null;
