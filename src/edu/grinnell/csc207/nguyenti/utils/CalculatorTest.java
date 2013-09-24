ae664631f37446f01a1f8dde2a3f81e10c2b9104
nguyenti <tn.xc.lax@gmail.com>
2013-09-24 16:55:59 -0500

    some testing

--- src/edu/grinnell/csc207/nguyenti/utils/CalculatorTest.java
+++ src/edu/grinnell/csc207/nguyenti/utils/CalculatorTest.java
@@ -9,11 +9,13 @@ public class CalculatorTest {
 	@Test
 	public void testEvaluate() throws Exception {
 		assertEquals("basic expression", 0, Calculator.evaluate("1 + 1").compareTo(new Fraction(2)));
+		assertEquals("storage", 0, Calculator.evaluate("r0 = 1 + 1").compareTo(Calculator.r[0]));
 		assertEquals("fractions", 0, Calculator.evaluate("1/2").compareTo(new Fraction(1, 2)));
 		assertEquals("multiply", 0, Calculator.evaluate("1 * 2").compareTo(new Fraction(2)));
 		assertEquals("divide", 0, Calculator.evaluate("1 / 2").compareTo(new Fraction(1, 2)));
 		assertEquals("subtract", 0, Calculator.evaluate("1 - 1/2").compareTo(new Fraction(1, 2)));
 		assertEquals("add", 0, Calculator.evaluate("1 + 2").compareTo(new Fraction(3)));
+		assertEquals("multiple storage", 0, Calculator.evaluate("r0 = r1 = 1 + 1").compareTo(Calculator.r[0]));
 	}
 
 }
